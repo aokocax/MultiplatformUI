354522a0567f5c2f119e3d1d009d8df285c0f630
Ahmet Oğuz Koca <aoguzkoca@hotmail.com>
2022-04-09 00:56:13 +0300

    Add project files.

---
+++ MauiSide/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+namespace MauiSide;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.RegisterBlazorMauiWebView()
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddBlazorWebView();
+		
+
+		return builder.Build();
+	}
+}
